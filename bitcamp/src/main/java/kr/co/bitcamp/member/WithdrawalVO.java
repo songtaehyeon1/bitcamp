476085fa9ad387c5d4de68841521013268fa4c11
hello105 <62743982+hello105@users.noreply.github.com>
2020-06-30 11:37:28 +0900

    Update WithdrawalVO.java

--- bitcamp/src/main/java/kr/co/bitcamp/member/WithdrawalVO.java
+++ bitcamp/src/main/java/kr/co/bitcamp/member/WithdrawalVO.java
@@ -6,41 +6,6 @@ public class WithdrawalVO {
 	private int userno;
 	private String username;
 	private String reason;	
-<<<<<<< HEAD
-	private String withdrawaldate;
-	public String getUserid() {
-		return userid;
-	}
-	public void setUserid(String userid) {
-		this.userid = userid;
-	}
-	public int getUserno() {
-		return userno;
-	}
-	public void setUserno(int userno) {
-		this.userno = userno;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getReason() {
-		return reason;
-	}
-	public void setReason(String reason) {
-		this.reason = reason;
-	}
-	public String getWithdrawaldate() {
-		return withdrawaldate;
-	}
-	public void setWithdrawaldate(String withdrawaldate) {
-		this.withdrawaldate = withdrawaldate;
-	}
-	
-	
-=======
 	private String reason_etc;
 	private String withdrawaldate;
 	
@@ -90,5 +55,4 @@ public class WithdrawalVO {
 	public void setWithdrawaldate(String withdrawaldate) {
 		this.withdrawaldate = withdrawaldate;
 	}
->>>>>>> jaeeunk
 }
